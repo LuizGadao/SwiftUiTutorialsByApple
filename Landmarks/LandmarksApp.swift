ca91324f1242a9a4b1d6587b2ae10eb7f5c9f8ad
Luiz <luizgadao@gmail.com>
2024-03-21 18:41:13 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Luiz Carlos Goncalves Dos Anjos on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
